7a575c462325605d3389151f3668971613f62ba7
Utsav Gurmachhan Magar <utsavmagar88728@gmail.com>
2022-07-19 10:25:46 +0545

    Complete exercise 2 and 3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (str, repeat) {
+    if (repeat < 0) {
+        return 'ERROR'
+    }
+	let reapeatedString = "";
+	for (let i = 0; i < repeat; i++) {
+		reapeatedString += str;
+	}
+	return reapeatedString;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (to_reverse) {
+	let reversedString = "";
+	for (let i = to_reverse.length - 1; i >= 0; --i) {
+		reversedString += to_reverse.at(i);
+	}
+	return reversedString;
 };
 
 // Do not edit below this line
