9c620cd036e8016d0a89f3f0230ad946bdedbb2e
Utsav Gurmachhan Magar <utsavmagar88728@gmail.com>
2022-07-20 08:39:59 +0545

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function () {
+	let toRemoveFrom = arguments[0];
+	for (let i = 1; i < arguments.length; i++) {
+		toRemoveFrom = toRemoveFrom.filter((element) => element !== arguments[i]);
+	}
+	return toRemoveFrom;
 };
 
 // Do not edit below this line
