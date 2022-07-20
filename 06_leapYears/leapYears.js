fdfe8edc82172aa99109e6a905407127f73203ad
Utsav Gurmachhan Magar <utsavmagar88728@gmail.com>
2022-07-20 08:52:09 +0545

    Complete exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,6 @@
-const leapYears = function() {
-
+const leapYears = function () {
+	let year = arguments[0];
+	return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
 };
 
 // Do not edit below this line
