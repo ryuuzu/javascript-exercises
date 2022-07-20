b69f4506efe5f4d4286f27f9e14e40df2e372b8d
Utsav Gurmachhan Magar <utsavmagar88728@gmail.com>
2022-07-20 08:45:48 +0545

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function (start, stop) {
+	let sum = 0;
+	if (
+		typeof start !== "number" ||
+		typeof stop !== "number" ||
+		start < 0 ||
+		stop < 0
+	) {
+		return "ERROR";
+	}
+	let actualStart, actualStop;
+	if (start > stop) {
+		actualStart = stop;
+		actualStop = start;
+	} else {
+		actualStart = start;
+		actualStop = stop;
+	}
+	for (i = actualStart; i <= actualStop; i++) {
+		sum += i;
+	}
+	return sum;
 };
 
 // Do not edit below this line
