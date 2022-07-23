690768e7851cb54f409b8facdf4ab0327c9e10ef
Utsav Gurmachhan Magar <utsavmagar88728@gmail.com>
2022-07-23 12:16:32 +0545

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+	let palindrome;
+	let finalString = str
+		.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+		.replace(/\s{2,}/g, " ")
+		.replace(/\s/g, "")
+		.toLowerCase();
+	let reversedString = "";
+	for (i = finalString.length - 1; i >= 0; i--) {
+		reversedString += finalString.charAt(i);
+	}
+	return reversedString === finalString;
 };
 
 // Do not edit below this line
