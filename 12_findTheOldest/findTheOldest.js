00448804754eba41de02f69253824df39463c606
Utsav Gurmachhan Magar <utsavmagar88728@gmail.com>
2022-07-23 12:37:48 +0545

    complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const getAge = function (person) {
+	if (person.yearOfDeath === undefined) {
+		return new Date().getFullYear() - person.yearOfBirth;
+	} else {
+		return person.yearOfDeath - person.yearOfBirth;
+	}
+};
 
+const findTheOldest = function (people) {
+	let oldest = people[0];
+	people.forEach((person) => {
+		if (getAge(person) > getAge(oldest)) {
+			oldest = person;
+		}
+	});
+	return oldest;
 };
 
 // Do not edit below this line
