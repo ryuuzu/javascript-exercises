6354f45294e10dce24de2c21301459d0171ced16
Utsav Gurmachhan Magar <utsavmagar88728@gmail.com>
2022-07-23 12:20:48 +0545

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function (end) {
+	if (end < 1) {
+		return "OOPS";
+	} else if (end == 1 || end == 2) {
+		return 1;
+	} else {
+		let a = 1;
+		let b = 1;
+		let c;
+		for (let i = 3; i <= end; i++) {
+			c = a + b;
+			a = b;
+			b = c;
+		}
+		return c;
+	}
 };
 
 // Do not edit below this line
